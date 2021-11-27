475ed3ed9ab71ec5a98d534d07f01b8b20ce9cba
dreamsofdesire <dreamsofdesire1991@gmail.com>
2021-11-27 16:44:06 +0530

    added py file and updated readme, added sample extracted story

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # litscrape
+A simple python utility for scraping story text from literotica url and exporting as txt file.  
+It supports extracting multiple pages in the same story and exports single txt file.  
+It supports new literotica website changes, working as tested on 27th-Nov-2021.  
+
+Requirements - python, beautifulsoup library, jupyter notebook (optional)
+  
+Instructions  
+Run litscrape.ipynb or litscrape.py and provide literotica story url as input 
---
+++ litscrape.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[2]:
+
+
+url= "https://www.literotica.com/s/oops-sorry-dean-flashes-wife-1"
+
+
+# In[3]:
+
+
+url = input ("Type literotica story url \nurl example - https://www.literotica.com/s/oops-sorry-dean-flashes-wife-1 \nType URL:")
+
+
+# In[4]:
+
+
+try:    
+    storyhtml = requests.get(url,headers={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
+    print('story found')
+    soup = BeautifulSoup(storyhtml.content, "lxml")
+except e:
+    import traceback
+    traceback.print_exc()
+
+
+# In[5]:
+
+
+fname= url.split("/s/")[-1] +".txt"
+fname
+
+
+# In[6]:
+
+
+lastpage=0
+try:
+    for pagedesc in soup.select('div[class*="panel clearfix"]'):
+        x = pagedesc.find_all("a")
+
+        for page in x:
+            lpstr = page.attrs['href']    
+            if "page=" in lpstr:
+                lastpage = int(lpstr.split("page=")[1])
+#         print(lastpage)
+except:
+    import traceback
+    traceback.print_exc()
+
+
+# In[7]:
+
+
+lastpage
+
+
+# In[8]:
+
+
+pages_to_fetch = []
+pages_to_fetch.append(url)
+for p in range(2, lastpage+1):        
+    if lastpage!=0:
+        tempurl = url + "?page=" + str(p)
+        pages_to_fetch.append(tempurl)
+
+
+# In[9]:
+
+
+pages_to_fetch
+
+
+# In[10]:
+
+
+def fetchpage(url):
+    story = "ERROR!!!"
+    try:       
+        storyhtml = requests.get(url,headers={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"})
+        print('fetching data from: ', url)
+        soup = BeautifulSoup(storyhtml.content, "lxml")
+        
+        for storypart in soup.select('div[class*="panel article"]'):
+            data = storypart.find_all('p')
+            story = "\n".join([p1.text for p1 in data])
+    except e:
+        import traceback
+        traceback.print_exc()
+
+    return story
+    
+
+
+# In[17]:
+
+
+import time
+fullstory = fname.split(".txt")[0]
+for page in pages_to_fetch:
+    story = fetchpage(page)
+#     time.sleep(1)
+    fullstory = fullstory + "\n\nSource:" + page + "\n\n" + story + "\n" 
+
+
+# In[18]:
+
+
+with open(fname, 'w') as f:
+    f.write(fullstory)
+    print("Story exported as :", fname)
+
+
+# In[ ]:
+
+
+
+
